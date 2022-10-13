e17419a154f98282c2db5b5af873570993e47e1f
pricoptudor <41987455+pricoptudor@users.noreply.github.com>
2022-10-13 13:34:22 +0300

    Gun 2 finished + added distance to score

--- Lab2/Assets/Scripts/Target.cs
+++ Lab2/Assets/Scripts/Target.cs
@@ -9,6 +9,9 @@ public class Target : MonoBehaviour
 
     private ParticleSystem particleSystem;
 
+    private Transform thrower1 = null;
+    private Transform thrower2 = null; 
+
     private void Awake()
     {
         particleSystem = gameObject.GetComponentInChildren<ParticleSystem>();
@@ -17,14 +20,26 @@ public class Target : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Tag: " + collision.gameObject.tag);
+        Debug.Log("Has gun? " + GameObject.FindGameObjectWithTag("Gun1"));
+
+        thrower1 = GameObject.FindGameObjectWithTag("Gun1").transform;
+        thrower2 = GameObject.FindGameObjectWithTag("Gun2").transform;
 
         if (collision.gameObject.CompareTag("Projectile"))
         {
-            ComputeOutScore(collision.transform.position);
+            ComputeOutScore(collision.transform.position, false, thrower1);
+        }
+        else if (collision.gameObject.CompareTag("Projectile2"))
+        {
+            ComputeOutScore(collision.transform.position, true, thrower2);
+        }
+        else //for any other object there is a minimum score when colliding
+        {
+            OnHit.Invoke(5);
         }
     }
 
-    private void ComputeOutScore(Vector3 hitPosition)
+    private void ComputeOutScore(Vector3 hitPosition, bool isVariant, Transform gunPosition)
     {
         float distanceFromCenter = Vector3.Distance(transform.position, hitPosition);
         int score = 0;
@@ -48,6 +63,17 @@ public class Target : MonoBehaviour
         else if(distanceFromCenter < 1.0f)
         {
             score = 10;
+        }
+
+        if (isVariant)
+        {
+            score *= 2;
+        }
+
+        if(gunPosition != null)
+        {
+            Debug.Log("Distance: " + (Vector3.Distance(gunPosition.position, hitPosition)));
+            score *= (int) (Vector3.Distance(gunPosition.position, hitPosition) * 10);
         }
 
         OnHit.Invoke(score);
